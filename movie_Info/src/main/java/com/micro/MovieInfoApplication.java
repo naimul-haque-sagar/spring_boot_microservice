fb28cac7f80b723666e5e44764cc4df97816a90c
naimul-haque-sagar <naimulhaquesagar@gmail.com>
2020-07-26 20:59:53 +0600

    eureka configured

--- movie_Info/src/main/java/com/micro/MovieInfoApplication.java
+++ movie_Info/src/main/java/com/micro/MovieInfoApplication.java
@@ -2,8 +2,10 @@ package com.micro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInfoApplication {
 
 	public static void main(String[] args) {
