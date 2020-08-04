d2490ff013c29131549f896c53f7e547e59c2a4f
naimul-haque-sagar <naimulhaquesagar@gmail.com>
2020-08-04 13:54:14 +0600

    hystrix command added

--- movie_service_Provider/src/main/java/com/micro/controller/MovieServiceProvider.java
+++ movie_service_Provider/src/main/java/com/micro/controller/MovieServiceProvider.java
@@ -2,6 +2,7 @@ package com.micro.controller;
 
 import com.micro.dto.*;
 import com.micro.mapper.MovieCastMapper;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.AllArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,6 +33,7 @@ public class MovieServiceProvider {
     static String MOVIE_RATINGS="http://movie-ratings/movie-ratings-consume/get/";
 
     @GetMapping("/getAllInfo/{movieName}")
+    @HystrixCommand(fallbackMethod = "getFallbackForAllInfo")
     public ResponseEntity<MovieInfoBundle> getAllInfo(@PathVariable String movieName){
         MovieInfoDto movieInfoDto=restTemplate.getForObject(MOVIE_INFO+"/{movieName}",MovieInfoDto.class,movieName);
 
@@ -42,5 +45,12 @@ public class MovieServiceProvider {
         return ResponseEntity.status(HttpStatus.OK).
                 body(new MovieInfoBundle(movieInfoDto.getReleaseYear(),movieInfoDto.getDirector(),movieInfoDto.getProducer()
        , movieCastDtos.stream().map(movieCastMapper::mapToDto).collect(Collectors.toList()),movieRatingsDto.getMovieRatings()));
+    }
+
+    public ResponseEntity<MovieInfoBundle> getFallbackForAllInfo(@PathVariable String movieName){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new MovieInfoBundle(2020,"director x","producer y",
+                        Arrays.asList(new MovieCast_Dto("Fallback",44,"male"),
+                                      new MovieCast_Dto("Fallback1",33,"male")), 4));
     }
 }
